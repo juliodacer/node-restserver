500b61c9b5410214c2eb68716434792d9f515677
Julio Davila <juliodacer@gmail.com>
2021-11-22 21:18:02 -0500

    start added

--- models/server.js
+++ models/server.js
@@ -5,7 +5,7 @@ class Server {
     constructor() {
         this.app = express();
         this.port = process.env.PORT;
-        this.usuariosPath = '/api/users';
+        this.usuariosPath = "/api/users";
 
         // Middlewares
         this.middlewares();
@@ -26,9 +26,7 @@ class Server {
     }
 
     routes() {
-
-        this.app.use(this.usuariosPath, require('../routes/users'));
-        
+        this.app.use(this.usuariosPath, require("../routes/users"));
     }
 
     listen() {
